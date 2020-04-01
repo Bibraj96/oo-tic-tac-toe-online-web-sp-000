3e9f56949e4baea1c2013ded3942a8f70dc8bfdc
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-01 14:35:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,7 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def WIN_COMBINATIONS [
+  WIN_COMBINATIONS [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -14,6 +14,5 @@ class TicTacToe
     [0, 4, 8],
     [2, 4, 6]
   ]
-end
 
 end
